0fc203dce396476074631bf8c57f2fa969b2d646
lentinicampusnobel-collab <lentini.campus.nobel@gmail.com>
2026-08-25 17:20:45 +0200

    Fix Resend Headers

--- app/api/send/route.ts
+++ app/api/send/route.ts
@@ -12,7 +12,7 @@ export async function POST(request: Request) {
     const response = await fetch('https://api.resend.com/emails', {
       method: 'POST',
       headers: {
-        Authorization: `Bearer ${process.env.RESEND_API_KEY}`,
+        Authorization: 'Bearer ' + process.env.RESEND_API_KEY,
         'Content-Type': 'application/json',
       },
       body: JSON.stringify({
