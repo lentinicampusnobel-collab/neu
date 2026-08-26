a56e08026db5ebd2a9e171df78d576d84e9d4dc7
lentinicampusnobel-collab <lentini.campus.nobel@gmail.com>
2026-08-26 12:17:27 +0200

    Fix API Key String

--- app/api/send/route.ts
+++ app/api/send/route.ts
@@ -13,7 +13,7 @@ export async function POST(req: Request) {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
-        'Authorization': `Bearer ${apiKey}`,
+        'Authorization': `Bearer ` + apiKey
       },
       body: JSON.stringify({
         from: 'onboarding@resend.dev',
